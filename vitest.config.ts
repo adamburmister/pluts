066ff14c5039ae26d95eab7029b621c88758a02f
Adam Burmister <adam.burmister@gmail.com>
2026-07-07 09:47:08 +1000

    improving vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    include: ['test/**/*.spec.ts'],
+    environment: "node",
+    include: ["test/**/*.spec.ts"],
     testTimeout: 30000,
     hookTimeout: 30000,
   },
